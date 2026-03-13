b67a7bdc616b6600558295a5d8affa487d211962
Lucian Ciuraru <0ciurg64@solent.ac.uk>
2026-03-13 21:50:45 +0000

    C# encapsulation using .Net properties

---
+++ Employee.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeApp.NetProperties
+{
+    class Employee
+    {
+        //Field Data or the state of the class
+        private string empName;
+        private int empId;
+        private float currPay;
+        // New field and property : Properties (as opposed to accessor and mutator methods) also make your types easier to
+        // manipulate, in that properties are able to respond to the intrinsic operators of C#.
+        private int empAge;
+        
+        //Properties!
+
+        public int Age
+        {
+            get { return empAge; }
+            set { empAge = value; }
+        }
+        public string Name
+        {
+            get { return empName; }
+            set
+            {
+                if (value.Length > 15)
+                    Console.WriteLine("Error! Name length exceeds 15 characters");
+                else
+                    empName = value;
+            }
+        }
+
+        // Updated constructors
+
+        public Employee(){}
+
+        public Employee( string name, int id, float pay)
+        :this (name, id, pay, 0) { } // Constructor chaining removes this duplication. Provides Default Values. The constructor that doesn't receive age automatically sets age to 0.
+
+        public Employee(string name, int id, float pay, int age)
+        {
+            empName = name;
+            empId = id;
+            currPay = pay;
+            empAge = age;
+        }
+
+
+        // Methods
+
+        public void GiveBonus (float amount)
+        {
+            currPay += amount;
+        }
+
+        public void DisplayStats()
+        {
+            Console.WriteLine("Name: {0}", empName);
+            Console.WriteLine("ID: {0}", empId);
+            Console.WriteLine("Pay: {0}", currPay);
+            Console.WriteLine("Age: {0}", empAge);
+        }
+        // The ’int’ represents the type of data this property
+        // encapsulates.
+
+        public int ID // Note lack of parentheses.
+
+        {
+            get { return empId;}
+            set { empId = value; }
+        }
+
+        public float Pay
+        {
+            get { return currPay; }
+            set {  currPay = value; }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeApp.NetProperties
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with Encapsulation *****\n");
+            Employee emp = new Employee("Lucian",100, 42000);
+            emp.GiveBonus(1000);
+            emp.DisplayStats();
+
+            // On Joe's birthday, you want to increment the age by one.
+            Employee joe = new Employee();
+            joe.Age++;
+
+            // On Joe's birthday, you want to increment the age by one. Using the traditional accessor and mutator methods.
+            /*
+             Employee joe = newEmployee();
+             joe.SetAge(joe.GetAge() + 1);
+            */
+
+
+            Console.ReadLine();
+        }
+    }
+}
